edd21530ddcd162c9ae0d1c9d5ed2707dc9476eb
bruce <bruce>
2020-12-03 01:15:34 +0800

    first commit

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/handler/BeanMethodJobHandler.java
@@ -0,0 +1,19 @@
+package com.xxl.job.executor.handler;
+
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanMethodJobHandler extends IJobHandler {
+    @XxlJob(value = "xxx", init = "myInit")
+    @Override
+    public void execute() throws Exception {
+        System.out.println("hello XXL_JOB");
+    }
+
+    public void myInit() {
+        System.out.println("我是自定義的 init 方法");
+    }
+
+}
