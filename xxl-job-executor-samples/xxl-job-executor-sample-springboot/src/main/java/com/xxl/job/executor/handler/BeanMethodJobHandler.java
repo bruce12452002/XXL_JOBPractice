6e15aa59ad200dc5c2528c26109bbba317c4a30f
bruce <bruce>
2020-12-06 23:45:38 +0800

    增加參數和子任務

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/handler/BeanMethodJobHandler.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/handler/BeanMethodJobHandler.java
@@ -1,6 +1,6 @@
 package com.xxl.job.executor.handler;
 
-import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +18,22 @@ public class BeanMethodJobHandler { //extends IJobHandler {
 
     @XxlJob(value = "ooo")
     public void execute2() throws Exception {
-        System.out.println("hello XXL_JOB2");
+        String jobParam = XxlJobHelper.getJobParam();
+        System.out.println("hello XXL_JOB2" + jobParam);
+    }
+
+    @XxlJob(value = "task1")
+    public void task1() throws Exception {
+        System.out.println("I'm task1");
+    }
+
+    @XxlJob(value = "task2")
+    public void task2() throws Exception {
+        System.out.println("I'm task2");
+    }
+
+    @XxlJob(value = "task3")
+    public void task3() throws Exception {
+        System.out.println("I'm task3");
     }
 }
